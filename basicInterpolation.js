494b1fd710c021e72048baea25e86423631acbfe
Olivor Hill <olivorhill@hotmail.com>
2024-08-15 11:48:09 +0100

    Changed the capitalizeName function to formatNamesAndAge
    Renamed the function that previously capitalized the names passed to the interpolation function - then added a piece of code that returns an integer if the age is given as a string. Comma swapped for a fullstop after the interpolated lastName.

--- basicInterpolation.js
+++ basicInterpolation.js
@@ -1,21 +1,23 @@
-const capitalizeName = (first, last) => {
+
+
+
+const formatNamesAndAge = (first, last, age) => {
     const firstName = first.charAt(0).toUpperCase() + first.substring(1).toLowerCase();
     const lastName = last.charAt(0).toUpperCase() + last.substring(1).toLowerCase();
-
-    return {firstName, lastName}
+    const ageToNum = Number.parseInt(age, 10);
+    return {firstName, lastName, ageToNum}
 }
 
 // the below calls the capitalizedName function - destructures the returned object from the above code
 // and passes those variables as an argument to the below basicInterpolation function
-const {firstName, lastName} = capitalizeName("oLIvor", "hILl");
-
+const {firstName, lastName, ageToNum} = formatNamesAndAge("oLIvor", "hILl", "40");
 
 
 const basicInterpolation = (firstName, lastName, age) => {
 
-    let interpolatedPhrase = `Hi, my name is ${firstName} ${lastName}, I am ${age} years old!`
+    let interpolatedPhrase = `Hi, my name is ${firstName} ${lastName}. I am ${age} years old!`
 
     return interpolatedPhrase
 }
 
-console.log(basicInterpolation(firstName, lastName, 30))
+console.log(basicInterpolation(firstName, lastName, ageToNum));
